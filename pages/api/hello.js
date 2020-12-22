107a6144cef56b72d076ec98dfbebf1b12d57ec5
McKlyde Lagnada <hello@mcklyde.com>
2020-12-21 16:12:56 -0800

    Setup firebase

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,24 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Example firebase connect stuff
+let admin = require('firebase-admin');
+
+
+
 
 export default (req, res) => {
+
+
+  admin
+  .auth()
+  .getUserByEmail("zeng@zeng.com")
+  .then((userRecord) => {
+    // See the UserRecord reference doc for the contents of userRecord.
+    res.json(userRecord.toJSON());
+  })
+  .catch(() => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
+
+  })
+
+
 }
